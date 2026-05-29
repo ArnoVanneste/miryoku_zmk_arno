a1e0ecb65ccc5b035c972abe18c596e6d1a6d669
Arno Vanneste <arno.vanneste@marelec.com>
2026-05-29 15:33:21 +0200

    Add Karma Tap layer escape

---
+++ config/karma_miryoku_overrides.h
@@ -0,0 +1,11 @@
+// Karma-specific Miryoku overrides.
+
+#pragma once
+
+#if defined(MIRYOKU_TAP_QWERTY) && !defined(MIRYOKU_LAYERS_FLIP)
+#define MIRYOKU_LAYER_TAP \
+&kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
+&kp A,             &kp S,             &kp D,             &kp F,             &kp G,             &kp H,             &kp J,             &kp K,             &kp L,             &kp SQT,           \
+&kp Z,             &kp X,             &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         &kp DOT,           &kp SLASH,         \
+U_NP,              U_NP,              &kp ESC,           &kp SPACE,         &kp TAB,           &kp RET,           &kp BSPC,          &u_to_U_BASE,      U_NP,              U_NP
+#endif
